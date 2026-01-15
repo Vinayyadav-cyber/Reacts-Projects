bece1bf448ee498eae6d874d28b0817690b3aa1b
Vinay Yadav <vinayyadav17072003@gmail.com>
2026-01-15 14:17:05 +0530

    Updated currency convertor

--- 04currencyConvertor/src/App.jsx
+++ 04currencyConvertor/src/App.jsx
@@ -8,7 +8,7 @@ import './index.css'
 function App() {
 
 
-  const [amount, setAmount] = useState("")
+  const [amount, setAmount] = useState(0)
   const [from, setFrom] = useState("usd")
   const [to, setTo] = useState("inr")
   const [convertedAmount, setConvertedAmount] = useState(0)
